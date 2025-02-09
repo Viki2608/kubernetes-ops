47324f7d339e38e224973b65ef157c47056945d4
Vignesh <vignesh26.manoharan@gmail.com>
2025-02-09 20:30:14 +0530

    Changed Region

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -39,7 +39,7 @@ module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v2.0.92"
 
   aws_region       = local.aws_region
-  azs              = ["us-west-2a", "us-west-2b"]
+  azs              = ["us-east-1a", "us-east-1b"]
   vpc_cidr         = "10.0.0.0/16"
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
